afd70b66909a32983879a56536b4c4848827dcd2
PerturbatorAndTonny <perturbatorandtonny@gmail.com>
2025-09-22 12:26:39 -0500

    feat(index): se genera path para las rutas de la api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express"
 import morgan from "morgan"
 
+import routerProducts from "./routes/product/product.route.js"
+
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
+
+app.use('/api', routerProducts)
 
 app.use((req, res, next) => {
     res.status(404).json({
